bc051f789a9088ee769ab13026ea396756d9214f
Shreyank Desai <shreyankdessai6@gmail.com>
2023-10-17 20:53:57 +0530

    First Commit done basic project setup, connected to database and created routes

---
+++ app.js
@@ -0,0 +1,92 @@
+const express =require("express")
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride =require("method-override");
+const ejsMate = require("ejs-mate");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+
+
+main()
+.then(()=>{
+  console.log("Connected to Database");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/RentIt');
+}
+
+app.get("/", (req, res)=>{
+  res.send("I am Groot");
+})
+
+//Index Route
+app.get("/listings", async(req, res)=>{
+  const allListings = await Listing.find({})
+  res.render("listings/index.ejs", {allListings});
+})
+
+// New Route
+app.get("/listings/new",(req,res)=>{
+  res.render("listings/new.ejs")
+})
+
+//Show Route
+app.get("/listings/:id", async(req, res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", {listing})
+})
+
+//Create Route
+app.post("/listings", async(req,res)=>{
+  const newListings = new Listing(req.body.listing);
+  await newListings.save();
+  res.redirect("listings")
+})
+
+//Edit Route
+app.get("/listings/:id/edit", async(req, res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", {listing});
+})
+
+//Update Route
+app.put("/listings/:id", async(req, res)=>{
+  let {id} =req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  res.redirect(`/listings/${id}`);
+})
+
+//Delete Route
+app.delete("/listings/:id", async(req, res)=>{
+  let {id} = req.params;
+  let deleteListing = await Listing.findByIdAndDelete(id);
+  console.log(deleteListing);
+  res.redirect("/listings")
+})
+
+// app.get("/testListing", async(req, res)=>{
+//   let sampleListing = new Listing({
+//     title: "My new Villa",
+//     description: "By the Beach",
+//     price: 1200,
+//     location: "Goa",
+//     country:"India"
+//   })
+
+//   await sampleListing.save();
+//   console.log("Sample was Saved");
+//   res.send("Successful")
+// })
+
+app.listen(9211, ()=>{
+  console.log("Server is listening on port 9211");
+})
---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose= require("mongoose")
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  image: { 
+    type: String,
+    default: "https://images.unsplash.com/photo-1696946775356-9c7d1a987104?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
+    set: (v) => v === " " ? "https://images.unsplash.com/photo-1696946775356-9c7d1a987104?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80" : v,
+  },
+  price: Number,
+  location: String,
+  country: String
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
