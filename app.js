a9e175670aaaac7a629bc594bcc8dc45ccef00b4
Shreyank Desai <shreyankdessai6@gmail.com>
2023-10-18 21:32:50 +0530

    added styling for main routes and also used bootstrap

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride =require("method-override");
-const ejsMate = require("ejs-mate");
+const engine = require("ejs-mate");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", engine);
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 
